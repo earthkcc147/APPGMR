408e6e0f2f89c5a04d4459e160070b8b175208fe
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-30 18:44:34 +0700

    Update run.py

--- run.py
+++ run.py
@@ -40,6 +40,7 @@ if __name__ == '__main__':
     
     # รอการกด Enter เพื่อดำเนินการต่อ
     input(Fore.GREEN + "\nกด Enter เพื่อดำเนินการต่อ...")
+    clear_console()
 
     # เรียกใช้ฟังก์ชัน login และตรวจสอบการล็อคอิน
     if login():
