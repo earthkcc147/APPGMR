13d8534f8fd59d5106032c7b0b58b0bd8ec2be7f
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-30 18:48:43 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 from colorama import init, Fore, Style
 from function.welcome import print_intro, print_logo
 from function.login import login  # นำเข้าฟังก์ชัน login จากไฟล์ login.py
+from function.main_menu import main_menu  # นำเข้าฟังก์ชัน main_menu จากไฟล์ main_menu.py
 
 # เริ่มต้นการใช้งาน colorama
 init(autoreset=True)
@@ -14,34 +15,14 @@ def clear_console():
         os.system('clear')
 
 
-def main_menu():
-    print("\nยินดีต้อนรับเข้าสู่ Main Menu")
-    print("1. รายการที่ 1")
-    print("2. รายการที่ 2")
-    print("3. ออกจากระบบ")
-
-    choice = input("กรุณาเลือกตัวเลือก: ")
-
-    if choice == '1':
-        print(Fore.CYAN + "คุณเลือก รายการที่ 1")
-    elif choice == '2':
-        print(Fore.CYAN + "คุณเลือก รายการที่ 2")
-    elif choice == '3':
-        print(Fore.YELLOW + "ออกจากระบบแล้ว")
-        exit()  # ออกจากโปรแกรม
-    else:
-        print(Fore.RED + "ตัวเลือกไม่ถูกต้อง")
-        main_menu()
-
-
 if __name__ == '__main__':
     clear_console()
     print_intro()
-    
+
     # รอการกด Enter เพื่อดำเนินการต่อ
     input(Fore.GREEN + "\nกด Enter เพื่อดำเนินการต่อ...")
     clear_console()
 
     # เรียกใช้ฟังก์ชัน login และตรวจสอบการล็อคอิน
     if login():
-        main_menu()
+        main_menu()  # เรียกใช้ฟังก์ชัน main_menu
