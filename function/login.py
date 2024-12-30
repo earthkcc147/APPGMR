0715c7ca0a4a1b77037c5f93f81bcfa0d853e6e3
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-30 18:37:24 +0700

    Update login.py

--- function/login.py
+++ function/login.py
@@ -3,6 +3,7 @@ import json
 import getpass
 from colorama import Fore
 from dotenv import load_dotenv
+from function.welcome import print_intro, print_logo
 
 # โหลดค่าจากไฟล์ .env
 load_dotenv()
@@ -26,6 +27,7 @@ def clear_console():
 
 
 def login():
+    print_logo()
     print("กรุณากรอกข้อมูลสำหรับล็อคอิน หรือพิมพ์ 'exit' เพื่อลงจากระบบ")
     
     username_input = input("Username: ")
