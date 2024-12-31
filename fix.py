8319fd267dfbb81af2cf7fd51bfa95e0588bc467
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-31 17:21:09 +0700

    Update

---
+++ fix.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+
+# กำหนดโมดูลที่ต้องการติดตั้งในตัวแปร modules
+modules = ['selenium', 'pyperclip']
+
+# ฟังก์ชันติดตั้งโมดูล
+def install_package(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# ตรวจสอบและติดตั้งโมดูล
+for module in modules:
+    try:
+        __import__(module)  # ลอง import โมดูล
+        print(f"{module} โมดูลถูกติดตั้งแล้ว.")
+    except ImportError:
+        print(f"กำลังติดตั้ง {module}...")
+        install_package(module)
+
+print("การติดตั้งเสร็จสมบูรณ์.")
