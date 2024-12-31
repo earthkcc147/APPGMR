243cbc70d4e51b598de2cd95549cb0025839c867
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-01 05:18:01 +0700

    Update computer

---
+++ function/computer/cctv.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+print('Check Update')
+os.system('git pull')
+os.system('clear')
+
+# Regular Colors
+black = "\033[0;30m"        # Black
+red = "\033[0;31m"          # Red
+green = "\033[0;32m"        # Green
+yellow = "\033[0;33m"       # Yellow
+blue = "\033[0;34m"         # Blue
+purple = "\033[0;35m"       # Purple
+cyan = "\033[0;36m"         # Cyan
+white = "\033[0;37m"        # White
+
+print('\n')
+print(green + ' |   ‌®®®     ®®®                    ')
+print(' |   ®®®     ®®®      [hack-cctv]            ')
+print(' |   ®®®     ®®®                ')
+print(' |   ®®®®®®®®®®®       ')
+print(' |   ®®®     ®®®          ')
+print(' |   ®®®     ®®®    ')
+print(' |   ®®®     ®®®    ')
+print('\n')
+print(green + '%37s' % '   |Devoloped By : Raz Ahamed|')
+print(red + '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(blue + 'Youtube Chanel: Real tips and Trikes 660')
+print(blue + 'Telegram Group: https://t.me/realtips6600')
+print(red + '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(green + '1) United States            47) Singapore (สิงคโปร์)')
+print('2) Japan (ญี่ปุ่น)           48) Iceland (ไอซ์แลนด์)')
+print('3) Italy (อิตาลี)           49) Malaysia (มาเลเซีย)')
+print('4) Korea (เกาหลี)           50) Colombia (โคลอมเบีย)')
+print('5) France (ฝรั่งเศส)       51) Tunisia (ตูนิเซีย)')
+print('6) Germany (เยอรมนี)       52) Estonia (เอสโตเนีย)')
+print('7) Taiwan (ไต้หวัน)        53) Dominican Republic (สาธารณรัฐโดมินิกัน)')
+print('')
+print(cyan + '8) Russian Federation (รัสเซีย)  54) Slovenia (สโลวีเนีย)')
+print('9) United Kingdom (สหราชอาณาจักร)  55) Ecuador (เอกวาดอร์)')
+print('10) Netherlands (เนเธอร์แลนด์)  56) Lithuania (ลิทัวเนีย)')
+print('11) Czech Republic (สาธารณรัฐเช็ก)  57) Palestinian (ปาเลสไตน์)')
+print('12) Turkey (ตุรกี)        58) New Zealand (นิวซีแลนด์)')
+print('13) Austria (ออสเตรีย)    59) Bangladesh (บังกลาเทศ)')
+print('14) Switzerland (สวิตเซอร์แลนด์)  60) Panama (ปานามา)')
+print('')
+print(red + '15) Spain (สเปน)         61) Moldova (มอลโดวา)')
+print('16) Canada (แคนาดา)      62) Nicaragua (นิการากัว)')
+print('17) Sweden (สวีเดน)      63) Malta (มอลตา)')
+print('18) Israel (อิสราเอล)    64) Trinidad And Tobago (ตรินิแดดและโตเบโก)')
+print('19) Iran (อิหร่าน)       65) Saudi Arabia (ซาอุดีอาระเบีย)')
+print('20) Poland (โปแลนด์)     66) Croatia (โครเอเชีย)')
+print('21) India (อินเดีย)      67) Cyprus (ไซปรัส)')
+print('22) Norway (นอร์เวย์)    69) United Arab Emirates (สหรัฐอาหรับเอมิเรตส์)')
+print('22) Norway (นอร์เวย์)    70) Kazakhstan (คาซัคสถาน)')
+print('25) Belgium (เบลเยียม)   71) Kuwait (คูเวต)')
+print('26) Brazil (บราซิล)     72) Venezuela (เวเนซุเอลา)')
+
+print('27) Bulgaria (บัลแกเรีย)   73) Georgia (จอร์เจีย)')
+print('28) Indonesia (อินโดนีเซีย)  74) Montenegro (มอนเตเนโกร)')
+print('')
+print(yellow + '29) Denmark (เดนมาร์ก)  75) El Salvador (เอลซัลวาดอร์)')
+print('30) Argentina (อาร์เจนตินา)  76) Luxembourg (ลักเซมเบิร์ก)')
+print('31) Mexico (เม็กซิโก)     77) Curacao (คูราเซา)')
+print('32) Finland (ฟินแลนด์)   78) Puerto Rico (เปอร์โตริโก)')
+print('33) China (จีน)          79) Costa Rica (คอสตาริกา)')
+print('34) South Africa (แอฟริกาใต้)  80) Belarus (เบลารุส)')
+print('35) South Africa (แอฟริกาใต้)  81) Albania (แอลเบเนีย)')
+print('36) Slovakia (สโลวาเกีย)   82) Liechtenstein (ลิกเตนสไตน์)')
+print('37) Hungary (ฮังการี)    83) Bosnia And Herzegovina (บอสเนียและเฮอร์เซโกวีนา)')
+print('')
+print(purple + '38) Ireland (ไอร์แลนด์)  84) Paraguay (ปารากวัย)')
+print('39) Egypt (อียิปต์)      85) Philippines (ฟิลิปปินส์)')
+print('40) Thailand (ไทย)       86) Faroe Islands (หมู่เกาะแฟโร)')
+print('41) Ukraine (ยูเครน)     87) Guatemala (กัวเตมาลา)')
+print('42) Serbia (เซอร์เบีย)  88) Nepal (เนปาล)')
+print('43) Hong Kong (ฮ่องกง)  89) Peru (เปรู)')
+print('44) Greece (กรีซ)       90) Uruguay (อุรุกวัย)')
+print('45) Portugal (โปรตุเกส)  91) Extra')
+print('46) Latvia (ลัตเวีย)     92) Andorra (อันดอร์รา)')
+print('')
+print(green + '93) Antigua And Barbuda (แอนติกาและบาร์บูดา)  00) subscribe')
+print('94) Armenia (อาร์เมเนีย) 120) Cayman Islands (หมู่เกาะเคย์แมน)')
+print('95) Angola (แองโกลา)    121) Laos (ลาว)')
+print('96) Australia (ออสเตรเลีย)  122) Lebanon (เลบานอน)')
+print('97) Aruba (อารูบา)      123) Sri Lanka (ศรีลังกา)')
+print('98) Azerbaijan (อาเซอร์ไบจาน)  124) Morocco (โมร็อกโก)')
+print('99) Barbados (บาร์เบโดส)   125) Madagascar (มาดากัสการ์)')
+print('')
+print(blue + '100) Bonaire (โบไนร์)    126) Macedonia (มาซิโดเนีย)')
+print('101) Bahamas (บาฮามาส)  127) Mongolia (มองโกเลีย)')
+print('102) Botswana (บอตสวานา)  128) Macao (มาเก๊า)')
+print('103) Congo (คองโก)       129) Martinique (มาร์ตินีก)')
+print('104) Ivory Coast (ไอวอรีโคสต์)  130) Mauritius (มอริเชียส)')
+print('105) Algeria (แอลจีเรีย)  131) Namibia (นามิเบีย)')
+print('106) Fiji (ฟิจิ)          132) New Caledonia (แคลโดเนียใหม่)')
+print('')
+print(purple + '107) Gabon (กาบอน)      133) Nigeria (ไนจีเรีย)')
+print('108) Guernsey (เกิร์นซีย์)  134) Qatar (กาตาร์)')
+print('109) Greenland (เกรนแลนด์)  135) Reunion (เรอูนียง)')
+print('110) Guadeloupe (กวาเดอลูป)  136) Sudan (ซูดาน)')
+print('111) Guam (กวม)           137) Senegal (เซเนกัล)')
+print('112) Guyana (กายานา)      138) Suriname (ซูรินาเม)')
+print('113) Honduras (ฮอนดูรัส)   139) Sao Tome And Principe (เซาท์โทเมและปรินซิเป)')
+print('')
+print(red + '114) Jersey (เจอร์ซีย์)   140) Syria (ซีเรีย)')
+print('115) Jamaica (จาไมกา)    141) Tanzania (แทนซาเนีย)')
+print('116) Jordan (จอร์แดน)     142) Uganda (ยูกันดา)')
+print('117) Kenya (เคนยา)       143) Uzbekistan (อุซเบกิสถาน)')
+print('118) Cambodia (กัมพูชา)  144) Saint Vincent (เซนต์วินเซนต์)')
+print('119) Saint Kitts (เซนต์คิตส์)  145) Benin (เบนิน)')
+
+import requests, re, colorama
+colorama.init()
+
+try:
+    print()
+    countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
+                 "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "PL", "IR",
+                 "NO", "RO", "IN", "VN", "BE", "BR", "BG", "ID", "DK", "AR",
+                 "MX", "FI", "CN", "CL", "ZA", "SK", "HU", "IE", "EG", "TH",
+                 "UA", "RS", "HK", "GR", "PT", "LV", "SG", "IS", "MY", "CO",
+                 "TN", "EE", "DO", "SI", "EC", "LT", "PS", "NZ", "BD", "PA",
+                 "MD", "NI", "MT", "TT", "SA", "HR", "CY", "PK", "AE", "KZ",
+                 "KW", "VE", "GE", "ME", "SV", "LU", "CW", "PR", "CR", "BY",
+                 "AL", "LI", "BA", "PY", "PH", "FO", "GT", "NP", "PE", "UY",
+                 "-" , "AD", "AG", "AM", "AO", "AU", "AW", "AZ", "BB", 
+                 "BQ", "BS", "BW", "CG", "CI", "DZ", "FJ", "GA", "GG", "GL",
+                 "GP", "GU", "GY", "HN", "JE", "JM", "JO", "KE", "KH", "KN",
+                 "KY", "LA", "LB", "LK", "MA", "MG", "MK", "MN", "MO", "MQ",
+                 "MU", "NA", "NC", "NG", "QA", "RE", "SD", "SN", "SR", "ST",
+                 "SY", "TZ", "UG", "UZ", "VC","BJ", ]
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
+
+    num = int(input("OPTIONS : "))
+    if num not in range(1, 145+1):
+        raise IndexError
+
+    country = countries[num-1]
+    res = requests.get(
+        f"http://www.insecam.org/en/bycountry/{country}", headers=headers
+    )
+    last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+
+    for page in range(int(last_page)):
+        res = requests.get(
+            f"http://www.insecam.org/en/bycountry/{country}/?page={page}",
+            headers=headers
+        )
+        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+        for ip in find_ip:
+            print("\033[1;31m", ip)
+except:
+    pass
+finally:
+    print("\033[1;37m")
+    exit()
+
+
+
+
