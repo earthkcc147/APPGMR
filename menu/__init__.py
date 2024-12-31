99f8fc26fcf029d862f632ed1eca5dbf7cdaba01
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-31 20:08:20 +0700

    Update __init__.py

--- menu/__init__.py
+++ menu/__init__.py
@@ -2,5 +2,6 @@ from .main_menu import main_menu
 from .menu_sms import show_sms_menu
 from .menu_email import show_email_menu
 from .menu_facebook import show_facebook_menu
+from .menu_discord import show_discord_menu
 from .menu_ip import show_ip_menu
 from .menu_gta import show_gta_menu
