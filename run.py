026107783f8ba62b627de0b52f5b36dc4f0f00d8
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-31 20:59:45 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import os
 from colorama import init, Fore, Style
 from banners import print_intro, print_logo
-from function import login  # นำเข้าฟังก์ชัน login จากไฟล์ login.py
-from menu import main_menu  # นำเข้าฟังก์ชัน main_menu จากไฟล์ main_menu.py
+from function import login 
+from menu import main_menu, menu_all
 
 
 # เริ่มต้นการใช้งาน colorama
@@ -23,7 +23,8 @@ if __name__ == '__main__':
     # รอการกด Enter เพื่อดำเนินการต่อ
     input(Fore.GREEN + "\nกด Enter เพื่อดำเนินการต่อ...")
     clear_console()
-    main_menu()
+    # main_menu()
+    menu_all()
 
     # เรียกใช้ฟังก์ชัน login และตรวจสอบการล็อคอิน
     # if login():
