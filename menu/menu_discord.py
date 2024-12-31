2c3c2170d87360cc8bddbd98854e307f50a2a6fa
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-31 19:44:11 +0700

    Update menu

---
+++ menu/menu_discord.py
@@ -0,0 +1,44 @@
+import subprocess
+import requests
+import os
+from banners import discord
+from colorama import Fore, Back, Style
+
+def clear_console():
+    # ตรวจสอบว่ากำลังทำงานในระบบปฏิบัติการใด
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # Linux หรือ macOS หรือ Termux
+        os.system('clear')
+
+# เมนู SMS
+def show_ip_menu():
+    while True:
+        clear_console()
+        discord()
+        print(Fore.CYAN + "\n📱 --- เมนู ddos & float --- 📱" + Style.RESET_ALL)
+        print(Fore.GREEN + "1. 🌏 Ddos & Float1" + Fore.YELLOW + " (ข้อความ) " + Style.RESET_ALL)
+        print(Fore.GREEN + "2. 🌎 Ddos & Float2" + Fore.YELLOW + " (คอมเม้นท์) " + Style.RESET_ALL)
+        print(Fore.YELLOW + "00. 🔙 ย้อนกลับ" + Style.RESET_ALL)
+
+        try:
+            sms_choice = int(input(Fore.BLUE + "\n🔔 กรุณาเลือกตัวเลือก: " + Style.RESET_ALL))
+
+            if sms_choice == 00:
+                print(Fore.YELLOW + "🔙 กลับสู่เมนูหลัก..." + Style.RESET_ALL)
+                from menu import main_menu  # นำเข้า main_menu
+
+                main_menu()  # กลับไปยัง main_menu
+                break
+            elif sms_choice == 1: 
+                print(Fore.GREEN + "กำลังรันไฟล์..." + Style.RESET_ALL)
+                subprocess.run(["python3", "function/ip/ip1.py"])
+            elif sms_choice == 2: 
+                print(Fore.GREEN + "กำลังรันไฟล์..." + Style.RESET_ALL)
+                subprocess.run(["python3", "function/ip/ip2.py"])
+
+
+            else:
+                print(Fore.RED + "❌ ตัวเลือกไม่ถูกต้อง กรุณาลองอีกครั้ง!" + Style.RESET_ALL)
+        except ValueError:
+            print(Fore.RED + "❌ กรุณากรอกตัวเลขเท่านั้น!" + Style.RESET_ALL)
