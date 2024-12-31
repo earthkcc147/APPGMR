67c5935f8f86025bc42f930ff781ac4d725d49eb
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2024-12-31 16:20:38 +0700

    Update main_menu.py

--- menu/main_menu.py
+++ menu/main_menu.py
@@ -1,6 +1,6 @@
 # menu/main_menu.py
 from colorama import Fore
-from menu.menu_sms import show_sms_menu  
+from menu import show_sms_menu, show_email_menu
 
 def main_menu():
     print("\nยินดีต้อนรับเข้าสู่ Main Menu")
