15907d6a9e9aa28d951dbd533ff49869f0712aec
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-05 17:12:42 +0700

    Update instagram

---
+++ function/instagram/ig_dowload.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+# ลิขสิทธิ์ (c) 2021 Fernando
+# URL: https://github.com/fernandod1/
+# ใบอนุญาต: MIT
+
+import sys
+import requests
+import time
+import urllib.request
+import os
+import json
+
+# รับชื่อผู้ใช้จาก input
+INSTAGRAM_USERNAME = input("กรุณาใส่ชื่อผู้ใช้ Instagram: ")
+
+# ------------------------------ไม่ต้องแก้ไขข้างล่างนี้--------------------------------------- #
+
+class Instagram_Downloader:
+    def __init__(self, username):
+        self.username = username
+        self.user_id = ""
+        self.jsondata = ""
+        self.apilabel = "graphql"
+        self.hash_timeline = ""
+
+    def create_download_directory(self):
+        try:
+            # สร้างโฟลเดอร์หลักถ้ายังไม่มี
+            if not os.path.exists('downloads'):
+                os.mkdir('downloads')
+                print("สร้างไดเรกทอรี 'downloads' สำเร็จ.")
+            
+            # สร้างโฟลเดอร์ของผู้ใช้ถ้ายังไม่มี
+            if not os.path.exists(f'downloads/{self.username}'):
+                os.mkdir(f'downloads/{self.username}')
+                print(f"สร้างไดเรกทอรี {self.username} สำเร็จในโฟลเดอร์ 'downloads'.")
+        except FileExistsError:
+            print(f"โฟลเดอร์ 'downloads' หรือ {self.username} มีอยู่แล้ว.")
+
+    def get_total_photos(self):
+        return int((self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["count"])) 
+
+    def set_user_id(self):
+        self.user_id = int(self.jsondata.json()[self.apilabel]["user"]["id"])
+
+    def get_end_cursor_timeline_media(self):
+        return self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["page_info"]["end_cursor"]
+    
+    def write_resume_end_cursor_timeline_media(self):
+        f = open("resume_"+self.username+".txt", "w")   # สร้างไฟล์บันทึกตำแหน่งที่ดาวน์โหลดล่าสุด
+        f.write(str(self.get_end_cursor_timeline_media()))
+        f.close()
+
+    def read_resume_end_cursor_timeline_media(self):
+        if os.path.isfile("resume_"+self.username+".txt"):
+            with open("resume_"+self.username+".txt") as f:
+                self.hash_timeline = f.readline().strip()
+                print ("****************\nเปิดโหมด Resume\n****************")
+                return True
+        else:
+            print ("ไม่มีโหมด Resume")
+            return False
+    
+    def remove_resume_file(self):
+        if os.path.exists("resume_"+self.username+".txt"):
+            os.remove("resume_"+self.username+".txt")
+
+    def set_apilabel(self,label):
+        self.apilabel = label
+    
+    def has_next_page(self):
+        return self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["page_info"]["has_next_page"]
+
+    def get_jsondata_instagram(self):
+        headers = {
+            "Host": "www.instagram.com",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+        }
+        if self.apilabel=="graphql":
+            self.jsondata = requests.get("https://www.instagram.com/" + self.username + "/feed/?__a=1", headers=headers)  
+            self.hash_timeline = self.get_end_cursor_timeline_media()
+            self.set_user_id()   
+        else:
+            self.jsondata = requests.get("https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" + str(self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D", headers=headers)
+            if user.has_next_page():
+                self.hash_timeline = self.get_end_cursor_timeline_media()
+        if (self.jsondata.status_code==200):
+            print("----------------\nโหลด URL Json สำเร็จ:\n----------------\nhttps://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%22" + str(self.user_id) + "%22%2C%22first%22%3A12%2C%22after%22%3A%22" + str(self.hash_timeline) + "%22%7D")
+        else:
+            print("ERROR: ไม่สามารถรับข้อมูล Json ได้")
+        return self.jsondata
+
+    def download_photos(self):
+        i = 0 
+        for match in self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["edges"]: 
+            time.sleep(0.5) 
+            print("กำลังดาวน์โหลดรูป:\n" +match["node"]["display_url"])
+            filename = f'downloads/{self.username}/{self.jsondata.json()[self.apilabel]["user"]["edge_owner_to_timeline_media"]["edges"][i]["node"]["shortcode"]}.jpg'
+            try:
+                if not os.path.exists(filename ):
+                    urllib.request.urlretrieve(match["node"]["display_url"], filename)
+                else:
+                    print("แจ้งเตือน: "+ filename+ " รูปนี้ดาวน์โหลดแล้ว ข้ามไป.")
+            except urllib.error.HTTPError as e:
+                print(str(e.code) + " ไม่สามารถดาวน์โหลดรูปได้")
+            i = i + 1
+
+    def download_videos(self):
+        i = 0 
+        if 'edge_felix_video_timeline' in self.jsondata.json()[self.apilabel]["user"]:
+            for match in self.jsondata.json()[self.apilabel]["user"]["edge_felix_video_timeline"]["edges"]: 
+                time.sleep(0.5) 
+                print("กำลังดาวน์โหลดวิดีโอ:\n" +match["node"]["video_url"])
+                filename = f'downloads/{self.username}/{self.jsondata.json()[self.apilabel]["user"]["edge_felix_video_timeline"]["edges"][i]["node"]["shortcode"]}.mp4'
+                try:
+                    if not os.path.exists(filename ):
+                        urllib.request.urlretrieve(match["node"]["video_url"], filename)
+                    else:
+                        print("แจ้งเตือน: "+ filename+ " วิดีโอนี้ดาวน์โหลดแล้ว ข้ามไป.")
+                except urllib.error.HTTPError as e:
+                    print(str(e.code) + " ไม่สามารถดาวน์โหลดวิดีโอได้")
+                i = i + 1
+ 
+
+# --------------------------------- โปรแกรมหลัก -------------------------------------#
+
+try:
+    user = Instagram_Downloader(INSTAGRAM_USERNAME)
+    user.create_download_directory()
+    user.get_jsondata_instagram()
+    user.download_photos()
+    user.download_videos()
+    user.set_apilabel("data")
+    user.read_resume_end_cursor_timeline_media()
+    while True:
+        time.sleep(5) # หยุดพักเพื่อหลีกเลี่ยงการถูกแบน
+        user.get_jsondata_instagram()
+        user.download_photos()
+        user.download_videos()
+        user.write_resume_end_cursor_timeline_media()
+        if user.has_next_page() == False:
+            user.remove_resume_file()
+            print("เสร็จสิ้น. ดาวน์โหลดรูปภาพ/วิดีโอทั้งหมดจากบัญชี "+INSTAGRAM_USERNAME+" เรียบร้อยแล้ว.")
+            break
+except:
+    print("แจ้งเตือน: สคริปต์หยุดทำงานก่อนกำหนดเนื่องจากจำนวนคำขอที่ส่งไปยัง Instagram API เกินขีดจำกัดประจำวัน.")
+    print("กรุณารันสคริปต์ใหม่อีกครั้งในอีกไม่กี่ชั่วโมงข้างหน้าเพื่อดำเนินการต่อจากที่ดาวน์โหลดค้างไว้.")
